bf000f04f349c335ef1fb9ad87a79ff97c54f484
Dave <dsoerensen28@gmail.com>
2020-05-13 15:26:42 -0400

    Commented out youtube stuff until quota refresh

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -166,7 +166,6 @@ apiController.getComplexRecipes = (req, res, next) => {
     japan: "Japanese",
   };
 
-
   let cuisineChoice;
   if (cuisineObj.hasOwnProperty(countryKey)) {
     cuisineChoice = cuisineObj[countryKey];
@@ -210,7 +209,7 @@ apiController.getTravelInfo = (req, res, next) => {
   let { city } = req.params;
   city = city.replace(" ", "%20");
 
-  const url = `https://api.sygictravelapi.com/1.2/en/places/list?query=${city}`;
+  const url = `https://api.sygictravelapi.com/1.2/en/places/list?limit=1&query=${city}`;
   const options = { headers: { "x-api-key": "pi9AODHpaqUOdUTgNweA7LbzxbJFKkD7O9fZ0We8" } };
   axios
     .get(url, options)
